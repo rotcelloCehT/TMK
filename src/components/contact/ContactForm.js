5e4d71d7800c05ee72eadb67052bf31cf86095b7
Tony Radin <anthonyradin123@gmail.com>
2021-08-11 10:39:36 +0300

    form v5

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -8,15 +8,15 @@ const ContactForm = () => {
         <input type="hidden" name="form-name" value="contact v1" />
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" required />
+          <input type="text" id="name" name="name" required />
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" name="email" required />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea id="message" required />
+          <textarea id="message" name="message" required />
         </div>
         <button type="submit">test</button>
       </form>
