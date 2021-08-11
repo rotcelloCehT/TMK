2c1798a35615d41667501bd32ed21cf50044e231
Tony Radin <anthonyradin123@gmail.com>
2021-08-11 10:29:44 +0300

    form v4

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -4,7 +4,8 @@ import { StyledContactForm } from "./ContactForm.Styled";
 const ContactForm = () => {
   return (
     <StyledContactForm>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="post" data-netlify="true" onSubmit="submit">
+        <input type="hidden" name="form-name" value="contact v1" />
         <div>
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" required />
