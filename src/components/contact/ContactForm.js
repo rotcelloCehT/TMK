85fe4bf4c96b0708a9b43c0c4174e12caa3a2b5d
Tony Radin <anthonyradin123@gmail.com>
2021-08-11 15:35:53 +0300

    styled form

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,47 +1,49 @@
 import React from "react";
-// import { StyledContactForm } from "./ContactForm.Styled";
+import { StyledContactForm } from "./ContactForm.Styled";
 
 const ContactForm = () => {
   return (
-    <form 
-      name="contact v2"
-      method="post"
-      data-netlify="true"
-      onSubmit="submit"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="form-name" value="contact v2" />
+    <StyledContactForm>
+      <form 
+        name="contact v2"
+        method="post"
+        data-netlify="true"
+        onSubmit="submit"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="contact v2" />
 
-      <div hidden>
-        <input name="bot-field" />
-      </div>
+        <div hidden>
+          <input name="bot-field" />
+        </div>
 
-      <div>
-        <label>First name<br />
-            <input type="text" name="first-name" />
-        </label>
-      </div>
+        <div>
+          <label>First name<br />
+              <input type="text" name="first-name" />
+          </label>
+        </div>
 
-      <div>
-        <label>Last name<br />
-            <input type="text" name="last-name" />
-        </label>
-      </div>
+        <div>
+          <label>Last name<br />
+              <input type="text" name="last-name" />
+          </label>
+        </div>
 
-      <div>
-        <label htmlFor="email" >Email</label><br />
-        <input id="email" type="email" name="email" />
-      </div>
+        <div>
+          <label htmlFor="email" >Email</label><br />
+          <input id="email" type="email" name="email" />
+        </div>
 
-      <div>
-        <label>Any Comments?<br />
-          <textarea name="comments"></textarea>
-        </label>
-      </div>
+        <div>
+          <label>Any Comments?<br />
+            <textarea name="comments"></textarea>
+          </label>
+        </div>
 
-      <button type="submit">Submit The Results</button>
+        <button type="submit">Submit The Results</button>
 
-    </form>
+      </form>
+    </StyledContactForm>
   );
 };
 
