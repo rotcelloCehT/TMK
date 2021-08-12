5ba6208e7aa98dc05091b782bffdbd784f3b212b
Tony Radin <anthonyradin123@gmail.com>
2021-08-12 13:22:57 +0300

    Contact Form v1

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -4,48 +4,24 @@ import { StyledContactForm } from "./ContactForm.Styled";
 const ContactForm = () => {
   return (
     // <StyledContactForm>
-      <form 
-        name="contact v2"
-        method="post"
-        data-netlify="true"
-        onSubmit="submit"
-        data-netlify-honeypot="bot-field"
-        data-netlify-recaptcha="true"
-      >
-        <input type="hidden" name="form-name" value="contact v2" />
-
-        <div hidden>
-          <input name="bot-field" />
-        </div>
-
-        <div>
-          <label>First name<br />
-              <input type="text" name="first-name" />
-          </label>
-        </div>
-
-        <div>
-          <label>Last name<br />
-              <input type="text" name="last-name" />
-          </label>
-        </div>
-
-        <div>
-          <label htmlFor="email" >Email</label><br />
-          <input id="email" type="email" name="email" />
-        </div>
-
-        <div>
-          <label>Any Comments?<br />
-            <textarea name="comments"></textarea>
-          </label>
-        </div>
-
-        <div data-netlify-recaptcha="true"></div>
-
-        <button type="submit">Submit The Results</button>
-
-      </form>
+    <form name="contact" method="post">
+    <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="name">Name</label> <br />
+        <input type="text" id="name" name="name" required />
+      </p>
+      <p>
+        <label htmlFor="email">Email</label> <br />
+        <input type="email" id="email" name="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message</label> <br />
+        <textarea id="message" name="message" required></textarea>
+      </p>
+      <p>
+        <input type="submit" value="Submit message" />
+      </p>
+    </form>
     // </StyledContactForm>
   );
 };
